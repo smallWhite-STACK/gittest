53872b7e1b1e535ddacf6e15e4d8c5d702ed57d0
xujian <xujian@qq.com>
2021-11-07 15:40:41 +0800

    徐健first commit

---
+++ src/main/java/XJdemo01/GitTest.java
@@ -0,0 +1,11 @@
+package XJdemo01;
+
+/**
+ * @author xujian
+ * 2021-11-07-15:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你是光，你是电，你是唯一的神话");
+    }
+}
